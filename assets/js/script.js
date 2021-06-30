e0c9e0f6a4fa0bb6dcb095905253d3f7884774b7
Matthew Edwards <m.t.edwards410@gmail.com>
2021-06-30 10:49:29 +0000

    enable search functionality for API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,51 @@
+function readyDocument() {
+	console.log("Ready");
+}
+$(document).ready(readyDocument);
 
+function replaceCommasSpaces(string) {
+	replaced = string.replace(/,/g, '%2C').replace(/ /g, '%20');
+	return replaced
+}
 
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${query}&diet=${diet}&excludeIngredients=${excludeIngredients}&intolerances=${intolerances}&number=10&offset=0&type=${type}&cuisine=${cuisine}`,
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "f51cb857c1mshe4a2ebcb218aee9p158115jsn9122b44a255a",
-		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-	}
-};
+function processCheckbox(check) {
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+	var checkboxArr = [];
+	$(check).children('input[type=checkbox]').each(function () {
+		if ((this).checked) {
+			checkboxArr.push(this.labels[0].textContent);
+		}
+	});
+	checkboxString = checkboxArr.toString();
+	checkboxString = replaceCommasSpaces(checkboxString);
+	return checkboxString
+}
+
+function compileUrl() {
+	cuisine = processCheckbox($("#cuisine"))
+	intolerances = processCheckbox($("#intolerance"))
+	diet = replaceCommasSpaces($("#diet").val());
+	query = replaceCommasSpaces($("#query-include").val());
+	excludeIngredients = replaceCommasSpaces($("#query-exclude").val());
+	type = replaceCommasSpaces($("#dish-type").val());
+	url = `search?query=${query}&diet=${diet}&excludeIngredients=${excludeIngredients}&intolerances=${intolerances}&number=10&offset=0&type=${type}&cuisine=${cuisine}`
+	return url
+}
+
+function executeSearch(cb) {
+	const settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${compileUrl(cb)}`,
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-key": "f51cb857c1mshe4a2ebcb218aee9p158115jsn9122b44a255a",
+			"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+		}
+	};
+
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+	});
+
+}
